1fef341dc77f7414888f09f6d17f55ea695cb586
djviodes <djviodes26@gmail.com>
2020-11-25 19:19:03 -0800

    Finished MVP

--- api/server.js
+++ api/server.js
@@ -1,9 +1,17 @@
 const express = require("express");
 
-const db = require("../data/dbConfig.js");
+const AccountRouter = require('../accounts/accounts-router');
 
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/accounts', AccountRouter);
+
+server.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'The API is up and running',
+    });
+});
 
 module.exports = server;
